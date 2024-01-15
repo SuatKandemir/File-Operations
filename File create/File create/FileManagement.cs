bd5c8be6c29896d4cf2bc75e8ef93d267de019c1
SuatKandemir <146870954+SuatKandemir@users.noreply.github.com>
2024-01-15 17:01:47 +0300

    Compleated

---
+++ File create/File create/FileManagement.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FileManagementApplication
+{
+    public class FileManagement
+    {
+    }
+    public class FileManagements
+    {
+
+
+          public static void CreateFile()
+        {
+
+            Console.WriteLine("Please enter the file name you want to create");
+            string filename = Console.ReadLine();
+            if (!File.Exists(filename))
+            {
+                File.Create(filename).Close();
+                Console.WriteLine($"{filename} Your file has been created ");
+            }
+            else
+            {
+                Console.WriteLine($"{filename}: Named file has already been created before");
+            }
+        }
+         public static void FileReading()
+        {
+            Console.WriteLine("Please enter the name of the file you want to read");
+            string filesname = Console.ReadLine();
+            if (File.Exists(filesname))
+            {
+                string content = File.ReadAllText(filesname);
+                Console.WriteLine($"Dosya içeriği \n {content}");
+            }
+            else
+            {
+                Console.WriteLine($"{filesname} Your forensic file was not found");
+            }
+        }
+
+        public static void FileWriting()
+        {
+            Console.WriteLine("Please enter the name of the file you want to print");
+            string filname = Console.ReadLine();
+
+            Console.WriteLine("Enter the text you want to write to the file");
+            string content = Console.ReadLine();
+
+            File.WriteAllText(filname, content);
+            Console.WriteLine($"{filname} to the file named file.");
+        }
+
+        public static void FileDelete()
+        {
+            Console.WriteLine("Please enter the name of the file you want to delete");
+            string filename = Console.ReadLine();
+            if (File.Exists(filename))
+            {
+                Console.WriteLine($"{filename} the file is removed");
+            }
+            else
+            {
+                Console.WriteLine($" {filename} no such file found");
+            }
+
+        }
+
+    }
+}
---
+++ File create/File create/Program.cs
@@ -0,0 +1,44 @@
+﻿
+
+namespace FileManagementApplication
+{
+    class Program
+    {
+        static void Main()
+        {
+            Console.WriteLine("Please select the action you want to take ");
+            Console.WriteLine("1 - File Create");
+            Console.WriteLine("2 - File Reading ");
+            Console.WriteLine("3 - Flie Writing");
+            Console.WriteLine("4 - File Remove");
+            Console.WriteLine("5 - Exsit");
+
+            Console.WriteLine("Please make your selection ");
+            int choise = Convert.ToInt32(Console.ReadLine());
+            switch (choise) 
+            {
+                case 1:
+                    FileManagements.CreateFile();
+                    break;
+                case 2:
+                    FileManagements.FileReading();
+                    break;
+
+                case 3:
+                    FileManagements.FileWriting();
+                    break;
+
+                case 4:
+                    FileManagements.FileDelete();
+                    break;
+
+                default:
+                    Console.WriteLine("false value please again");
+                    break;
+
+            }
+
+        }
+
+    }
+}
